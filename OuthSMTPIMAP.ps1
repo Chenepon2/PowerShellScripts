18009e121fd506ceefaf59297a8b38072bf9db6d
Pedro Blanco <76795724+Chenepon2@users.noreply.github.com>
2024-10-13 23:07:30 +0100

    Add files via upload

---
+++ OuthSMTPIMAP.ps1
@@ -0,0 +1,18 @@
+﻿$AppId = ""
+$TenantId = ""
+ 
+Connect-AzureAd -Tenant $TenantId      
+($Principal = Get-AzureADServicePrincipal -filter "AppId eq '$AppId'")
+$PrincipalId = $Principal.ObjectId 
+ 
+$DisplayName = "IMAP/smtp" 
+ 
+Connect-ExchangeOnline
+ 
+New-ServicePrincipal -AppId $AppId -ServiceId $PrincipalId -DisplayName $DisplayName
+ 
+ Add-MailboxPermission -User $PrincipalId -AccessRights FullAccess -Identity "sharemialbox@contoso.com"
+ 
+ 
+Add-RecipientPermission -Trustee $PrincipalId -AccessRights "SendAs" -Identity "sharemialbox@contoso.com"
+
